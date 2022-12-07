3360ca6cb2736b119dbeaed4697abb1700a41527
Jip van Dalen <141040@reggesteyn.nu>
2022-12-07 14:57:49 +0100

    Fix

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<Item> itemList = new List<Item>();
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            Inventory.instance.AddItem(itemList[Random.Range(0, itemList.Count)]);
+        }
+    }
+}
---
+++ Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    // VARIABLES
+    private bool inventoryOpen = false;
+    private List<ItemSlot> itemSlotList = new List<ItemSlot>();
+
+
+    // REFERENCES
+    public bool InventoryOpen => inventoryOpen;
+    public GameObject inventoryParent;
+    public GameObject inventoryTab;
+    public GameObject craftingTab;
+    public GameObject itemSlotPrefab;
+    public Transform inventoryItemTransform;
+
+    private void Start()
+    {
+        Inventory.instance.onItemChange += UpdateInventoryUI;
+        UpdateInventoryUI();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            if(inventoryOpen)
+            {
+                //close inventory
+                CloseInventory();
+            }
+            else
+            {
+                //open inventory
+                OpenInventory();
+            }
+        }
+    }
+
+    private void UpdateInventoryUI()
+    {
+        int currentItemCount = Inventory.instance.inventoryItemList.Count;
+
+        if(currentItemCount > itemSlotList.Count)
+        {
+            //Add move item slots
+            AddItemSlots(currentItemCount);
+        }
+
+        for(int i = 0; i < itemSlotList.Count; ++i)
+        {
+            if(i <= currentItemCount)
+            {
+                //update the current item in the slot
+                itemSlotList[i].AddItem(Inventory.instance.inventoryItemList[i]);
+            }
+            else
+            {
+                itemSlotList[i].DestroySlot();
+                itemSlotList.RemoveAt(i);
+            }
+        }
+    }
+
+    private void AddItemSlots(int currentItemCount)
+    {
+        int amount = currentItemCount - itemSlotList.Count;
+
+        for(int i = 0; i < amount; ++i)
+        {
+            GameObject GO = Instantiate(itemSlotPrefab, inventoryItemTransform);
+            ItemSlot newSlot = GO.GetComponent<ItemSlot>();
+            itemSlotList.Add(newSlot);
+        }
+    }
+
+    private void OpenInventory()
+    {
+        ChangeCursorState(false);
+        OnInventoryTabClicked();
+        inventoryOpen = true;
+        inventoryParent.SetActive(true);
+    }
+
+    private void CloseInventory()
+    {
+        ChangeCursorState(true);
+        inventoryOpen = false;
+        inventoryParent.SetActive(false);
+    }
+
+    public void OnCraftingTabClicked()
+    {
+        craftingTab.SetActive(true);
+        inventoryTab.SetActive(false);
+    }
+
+        public void OnInventoryTabClicked()
+    {
+        craftingTab.SetActive(false);
+        inventoryTab.SetActive(true);
+    }
+
+    private void ChangeCursorState(bool lockCursor)
+    {
+        if(lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+}

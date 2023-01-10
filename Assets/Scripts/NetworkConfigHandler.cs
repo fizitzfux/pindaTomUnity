098767770f5e8f8545019962329ff9148d2a2882
jippie13 <jippiegames12@gmail.com>
2023-01-10 21:50:12 +0100

    Add server toggle

--- Assets/Scripts/NetworkConfigHandler.cs
+++ Assets/Scripts/NetworkConfigHandler.cs
@@ -6,16 +6,20 @@ using Unity.Netcode.Transports.UTP;
 
 public class NetworkConfigHandler : MonoBehaviour
 {
+    public bool isServer = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        if (!NetworkConfig.Server) NetworkConfig.Server = isServer;
+
         // Set connection settings to configured settings
         GetComponent<UnityTransport>().ConnectionData.Address = NetworkConfig.ip;
         GetComponent<UnityTransport>().ConnectionData.Port = NetworkConfig.port;
 
         // Start Host, Server or Client depending on config
-        if (NetworkConfig.Host) NetworkManager.Singleton.StartHost();
-        else if (NetworkConfig.Server) NetworkManager.Singleton.StartServer();
+        if (NetworkConfig.Server) NetworkManager.Singleton.StartServer();
+        else if (NetworkConfig.Host) NetworkManager.Singleton.StartHost();
         #if UNITY_EDITOR // If in unity editor start host, dev only
             Debug.Log("Started Host because of editor");
             NetworkManager.Singleton.StartHost();
